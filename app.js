ecb07769360a98d72dfc6571c490a1469236465d
tgszwdv <102832569+tgszwdv@users.noreply.github.com>
2023-11-28 12:47:51 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
+function showCreateAccount() {
+  document.getElementById("login-type").style.display = "none";
+  document.getElementById("create-account-box").style.display = "block";
+  document.getElementById("login-email").style.display = "none";
+}
+
+function showLoginType() {
+  document.getElementById("create-account-box").style.display = "none";
+  document.getElementById("login-type").style.display = "block";
+  document.getElementById("login-email").style.display = "none";
+}
+
+function showLoginWithEmail() {
+  document.getElementById("create-account-box").style.display = "none";
+  document.getElementById("login-type").style.display = "none";
+  document.getElementById("login-email").style.display = "block";
+}
+
+
 
 const firebaseConfig = {
-  
+
    apiKey: "AIzaSyB2OkyEL8IwEJVm39guJ2uKDRnFbGrSuNs",    
     authDomain: "organizer-c70eb.firebaseapp.com",
     projectId: "organizer-c70eb",
@@ -14,22 +33,18 @@ firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 var database = firebase.database();
 
-
+// id user
 auth.onAuthStateChanged((user) => {
-  const loginForm = document.getElementById('loginForm');
   if (user) {
-    // User is signed in
-    console.log('User is already signed in:', user);
-    showMenu();
-    // Hide the login form
-    loginForm.style.display = 'none';
+    console.log('Logado:', user);
+    window.location.href = '/menu/';
   } else {
-    // No user is signed in
-    console.log('No user is signed in.');
-    // Show the login form
-    loginForm.style.display = 'flex';
+    console.log('Usuario não Logado.');
+
   }
 });
+
+
 
 // Função para autenticar com o Google
 function signInWithGoogle() {
@@ -57,43 +72,54 @@ function signInWithEmail() {
       // Usuário autenticado com sucesso
       const user = userCredential.user;
       console.log('Usuário autenticado com e-mail e senha:', user);
-      showMenu();
+      window.location.href = '/menu/';
     })
     .catch((error) => {
       console.error('Erro ao autenticar usuário com e-mail e senha:', error.message);
     });
 }
 
-// Função para criar uma conta
-function createAccount() {
-    const email = document.getElementById('createEmail').value;
-    const password = document.getElementById('createPassword').value;
 
+function createAccountWithEmail() {
+  const name = document.getElementById('createNome').value;
+  const email = document.getElementById('createEmail').value;
+  const password = document.getElementById('createPassword').value;
+
+  // Verifica se todos os campos foram preenchidos
+  if (name && email && password) {
+    // Cria a conta no Firebase
     auth.createUserWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-            // Usuário criado com sucesso
-            const user = userCredential.user;
-            console.log('Usuário criado com e-mail e senha:', user);
-            showMenu();
-            hideCreateAccountForm();
-        })
-        .catch((error) => {
-            console.error('Erro ao criar usuário com e-mail e senha:', error.message);
-        });
+      .then((userCredential) => {
+        // Usuário criado com sucesso
+        const user = userCredential.user;
+
+        // Chama a função para atualizar o nome do perfil
+        updateProfileName(user, name);
+
+        // Redireciona para a página '/menu'
+        window.location.href = '/menu/';
+      })
+      .catch((error) => {
+        console.error('Erro ao criar conta com e-mail e senha:', error.message);
+      });
+  } else {
+    console.error('Por favor, preencha todos os campos.');
+  }
 }
 
-function showCreateAccountForm() {
-    document.getElementById('createAccountContainer').style.display = 'block';
-    document.getElementById('loginForm').style.display = 'none';
+// Função para atualizar o nome do perfil
+function updateProfileName(user, name) {
+  // Atualiza o perfil do usuário com o nome fornecido
+  user.updateProfile({
+    displayName: name
+  })
+  .then(() => {
+    // Nome do usuário atualizado com sucesso
+    console.log('Nome do perfil atualizado:', user.displayName);
+  })
+  .catch((updateError) => {
+    console.error('Erro ao atualizar o nome do perfil:', updateError.message);
+  });
 }
 
-function hideCreateAccountForm() {
-    document.getElementById('createAccountContainer').style.display = 'none';
-    document.getElementById('loginForm').style.display = 'none';  // or set it to 'block' if you want to show the login form again
-}
 
-// Função para mostrar o menu após o login
-function showMenu() {
-  document.getElementById('loginForm').style.display = 'none';
-  window.location.href = 'https://tgszwdv.github.io/organize/menu';
-}
